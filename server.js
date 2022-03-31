09a6583adfc41e7894256dee53c4b3984a91ccbe
TContextEXE <tobias03altagracia@gmail.com>
2022-03-30 21:34:38 -0300

    First Commit

---
+++ server.js
@@ -0,0 +1,32 @@
+//Imports
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const indexRouter = require('./routes/index');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+//MongoDB setup
+mongoose.connect(process.env.DB_URL);
+const db = mongoose.connection;
+db.on('error', error=>{
+    console.log(error)
+});
+db.once('open', ()=>{
+    console.log('Connected to database')
+})
+
+//Express setup
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/layout');
+
+app.use(expressLayouts);
+app.use(express.static('public'));
+app.use('/', indexRouter);
+
+
+app.listen(4000, ()=>{
+    console.log('Listening on port 4000');
+});
